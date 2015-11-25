8e4702193ba38d17ef222211fce23ae00a801d4b
Colin McKenna <colinjmckenna@gmail.com>
2015-11-24 19:39:29 -0800

    adjust routes & views

--- apps/events/urls.py
+++ apps/events/urls.py
@@ -4,6 +4,6 @@ from apps.events import views
 urlpatterns = patterns ('',
 	url(r'^$', views.index, name="events_index"),
 	url(r'^show/', views.show, name="show"),
-	url(r'^create/', views.create, name="create"),
+	url(r'^create', views.create, name="create"),
 	url(r'^city/', views.get_city, name="city")
 )
--- apps/events/views.py
+++ apps/events/views.py
@@ -2,28 +2,18 @@ from django.shortcuts import render, redirect
 import requests, json
 
 def index(request):
+	pass
 
-	url = "https://api.locu.com/v1_0/venue/search/?locality=Bellevue&region=Wa&category=restaurant&open_at=2015-11-26&api_key=ba6050865a98a654d2fa32c1b823f5769000dd77"
-	restaurants = {
-		"restaurants": json.loads(requests.get(url).content)
-		}
-	try:
-		request.session["city"]
-	except:
-		request.session["city"] = "Seattle"
-	city = request.session["city"]
-	url2 = "https://api.locu.com/v1_0/venue/search/?locality="+city+"&region=Wa&category=restaurant&open_at=2015-11-26&api_key=ba6050865a98a654d2fa32c1b823f5769000dd77"
-	places = {
-		"places": json.loads(requests.get(url2).content)
-		}
-	return render(request, "events/index.html", places)
 def get_city(request):
-	request.session["city"] = request.POST["city"]
-	print request.session["city"]
-	return redirect("/events/")
+	request.session['city'] = request.POST["city"]
+	url = "https://api.locu.com/v1_0/venue/search/?locality="+request.session['city']+"&region=Wa&category=restaurant&open_at=2015-11-26&api_key=ba6050865a98a654d2fa32c1b823f5769000dd77"
+	places = {
+		"places": json.loads(requests.get(url).content)
+		}
+	return render(request, "events/create.html", places)
 
 def show(request):
   pass
 
 def create(request):
-  pass
+	return render(request, 'events/create.html')
