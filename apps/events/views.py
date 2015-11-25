29103d92631076f3626267d581f6af158ba827a5
Sarah Sherman <sarsherman@outlook.com>
2015-11-25 10:03:39 -0800

    link user to event page

--- apps/events/views.py
+++ apps/events/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 import requests, json
-# from apps.events import Event, Reservation
+from apps.events.models import Event, Reservation
+from apps.login.models import User
 
 def index(request):
 	return render(request, 'events/index.html')
