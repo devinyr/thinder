c43b6008a8acc4411e9fa773b57fddc355b1430a
Colin McKenna <colinjmckenna@gmail.com>
2015-11-24 17:35:24 -0800

    stash

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 Kelin & Colin & Emmet & Devi & Sarah were here
 
-YEah we all were here :)
+Yeah we all were here :)
--- apps/events/views.py
+++ apps/events/views.py
@@ -2,14 +2,15 @@ from django.shortcuts import render
 import requests
 
 def index(request):
-	url = "https://api.locu.com/v1_0/venue/search/?locality=Bellevue&region=Wa&open_at=2015-11-26&api_key=ba6050865a98a654d2fa32c1b823f5769000dd77"
-	restaurants = {
-		"restaurants": requests.get(url).content
-		}
-	return render(request, "events/index.html", restaurants)
+  url = "https://api.locu.com/v1_0/venue/search/?locality=Bellevue&region=Wa&open_at=2015-11-26&api_key=ba6050865a98a654d2fa32c1b823f5769000dd77"
+
+  food = requests.get(url).content
+  place = food[0]
+  print place
+  return render(request, "events/index.html", food)
 
 def show(request):
-	return render(request, "events/show.html")
+  pass
 
 def create(request):
-	return render(request, "events/create.html")
+  pass
