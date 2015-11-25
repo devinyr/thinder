f30b312b24ce176cb280c771c0e7be02b7760f4d
Emmett Agnew <eragnew@gmail.com>
2015-11-25 13:37:33 -0800

    Added events list

--- apps/events/urls.py
+++ apps/events/urls.py
@@ -8,4 +8,5 @@ urlpatterns = patterns ('',
 	url(r'^city/', views.get_city, name="city"),
   url(r'^create', views.create, name='create'),
   url(r'^commit', views.commit, name='commit'),
+  url(r'^my_events$', views.redirected_home, name='redirected_home')
 )
